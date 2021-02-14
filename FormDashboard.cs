378feb875057a83807d97ec4c9a9e7da16ebbc80
Baguslana <59597696+Baguslana@users.noreply.github.com>
2021-02-14 10:40:56 +0700

    Menambahkan user control

---
+++ FormDashboard.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Luthor.lib;
+using System.Windows.Forms;
+
+namespace Bayarin
+{
+    public partial class FormDashboard : Form
+    {
+        public FormDashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void FormDashboard_Load(object sender, EventArgs e)
+        {
+            /*Jika login sebagai petugas*/
+            if (Session.getUserLogged().Columns.Contains("level") && Session.getUserLogged().Rows[0].Field<string>("level") == "officer")
+            {
+                gunaLabel3.Text = "Petugas";
+            }
+
+            /*Jika login sebagai siswa*/
+            if (!Session.getUserLogged().Columns.Contains("level"))
+            {
+                gunaLabel3.Text = "Siswa";
+
+                label2.Visible = false;
+                gunaButton2.Visible = false;
+
+                label3.Visible = false;
+                gunaButton3.Visible = false;
+
+                /*label4.Location = new System.Drawing.Point(12, 185);*/
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            var confirm = MessageBox.Show("Sign Out", "Yakin ingin keluar?", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (confirm == DialogResult.Yes)
+            {
+                Auth.Logout();
+                this.Hide();
+                Form login = new Login();
+                login.Show();
+            }
+        }
+    }
+}
--- Form_Dashboard.Designer.cs
+++ Form_Dashboard.Designer.cs
@@ -28,12 +28,108 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.gunaLabel1 = new Guna.UI.WinForms.GunaLabel();
+            this.gunaLinePanel1 = new Guna.UI.WinForms.GunaLinePanel();
+            this.gunaLinePanel2 = new Guna.UI.WinForms.GunaLinePanel();
+            this.gunaLabel3 = new Guna.UI.WinForms.GunaLabel();
+            this.gunaLabel2 = new Guna.UI.WinForms.GunaLabel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.gunaLinePanel1.SuspendLayout();
+            this.gunaLinePanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gunaLabel1
+            // 
+            this.gunaLabel1.AutoSize = true;
+            this.gunaLabel1.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gunaLabel1.Location = new System.Drawing.Point(37, 18);
+            this.gunaLabel1.Name = "gunaLabel1";
+            this.gunaLabel1.Size = new System.Drawing.Size(116, 34);
+            this.gunaLabel1.TabIndex = 1;
+            this.gunaLabel1.Text = "Bayarin";
+            // 
+            // gunaLinePanel1
+            // 
+            this.gunaLinePanel1.BackColor = System.Drawing.Color.White;
+            this.gunaLinePanel1.Controls.Add(this.gunaLabel1);
+            this.gunaLinePanel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.gunaLinePanel1.LineColor = System.Drawing.Color.Black;
+            this.gunaLinePanel1.LineStyle = System.Windows.Forms.BorderStyle.None;
+            this.gunaLinePanel1.Location = new System.Drawing.Point(0, 0);
+            this.gunaLinePanel1.Name = "gunaLinePanel1";
+            this.gunaLinePanel1.Size = new System.Drawing.Size(200, 673);
+            this.gunaLinePanel1.TabIndex = 2;
+            // 
+            // gunaLinePanel2
+            // 
+            this.gunaLinePanel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(103)))), ((int)(((byte)(119)))), ((int)(((byte)(239)))));
+            this.gunaLinePanel2.Controls.Add(this.gunaLabel3);
+            this.gunaLinePanel2.Controls.Add(this.gunaLabel2);
+            this.gunaLinePanel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.gunaLinePanel2.LineColor = System.Drawing.Color.Black;
+            this.gunaLinePanel2.LineStyle = System.Windows.Forms.BorderStyle.None;
+            this.gunaLinePanel2.Location = new System.Drawing.Point(200, 0);
+            this.gunaLinePanel2.Name = "gunaLinePanel2";
+            this.gunaLinePanel2.Size = new System.Drawing.Size(1062, 80);
+            this.gunaLinePanel2.TabIndex = 3;
+            // 
+            // gunaLabel3
+            // 
+            this.gunaLabel3.AutoSize = true;
+            this.gunaLabel3.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gunaLabel3.Location = new System.Drawing.Point(188, 18);
+            this.gunaLabel3.Name = "gunaLabel3";
+            this.gunaLabel3.Size = new System.Drawing.Size(105, 34);
+            this.gunaLabel3.TabIndex = 3;
+            this.gunaLabel3.Text = "Admin";
+            // 
+            // gunaLabel2
+            // 
+            this.gunaLabel2.AutoSize = true;
+            this.gunaLabel2.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gunaLabel2.Location = new System.Drawing.Point(93, 18);
+            this.gunaLabel2.Name = "gunaLabel2";
+            this.gunaLabel2.Size = new System.Drawing.Size(89, 34);
+            this.gunaLabel2.TabIndex = 2;
+            this.gunaLabel2.Text = "Role :";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(200, 80);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1062, 593);
+            this.panel1.TabIndex = 4;
+            // 
+            // Form_Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form2";
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(1262, 673);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.gunaLinePanel2);
+            this.Controls.Add(this.gunaLinePanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form_Dashboard";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FormDashboard";
+            this.gunaLinePanel1.ResumeLayout(false);
+            this.gunaLinePanel1.PerformLayout();
+            this.gunaLinePanel2.ResumeLayout(false);
+            this.gunaLinePanel2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Guna.UI.WinForms.GunaLabel gunaLabel1;
+        private Guna.UI.WinForms.GunaLinePanel gunaLinePanel1;
+        private Guna.UI.WinForms.GunaLinePanel gunaLinePanel2;
+        private System.Windows.Forms.Panel panel1;
+        private Guna.UI.WinForms.GunaLabel gunaLabel3;
+        private Guna.UI.WinForms.GunaLabel gunaLabel2;
     }
 }
